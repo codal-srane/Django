05c260636b6951cec55b8987f2f7ae919ea4c966
Sandeep Rane <srane@codal.com>
2019-08-07 10:07:50 -0500

    Created a test django project

---
+++ Django_Test/test_project/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sandeep/Documents/Practice/Django_Test/test_project/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
